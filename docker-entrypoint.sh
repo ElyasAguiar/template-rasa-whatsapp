ba9a6906a05dc7606fbd911476609c1d509db18a
ElyasAguiar <elyas.santana98@academico.ifs.edu.br>
2023-11-10 14:59:33 -0300

    config: add credentials and endpoints

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,4 @@ echo "Starting docker-entrypoint.sh"
 set -e
 
 echo "Starting Core..."
-rasa run --endpoints endpoints.yml --credentials credentials.yml -m app/models/prod_model.tar.gz --enable-api -p 5005 --cors "*"
+rasa run --endpoints app/endpoints.yml --credentials app/credentials.yml -m app/models/prod_model.tar.gz --enable-api -p 5005 --cors "*"
