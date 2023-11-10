599108fb8d902e3814065bea7c08b25d20637bf0
ElyasAguiar <elyas.santana98@academico.ifs.edu.br>
2023-11-10 16:14:29 -0300

    config: path rasa comand

--- core/main.py
+++ core/main.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # start the rasa server
-rasa_command = f'rasa run --endpoints endpoints.yml --credentials credentials.yml -m models/prod_model.tar.gz --enable-api -p 5005 --cors "*"'
+rasa_command = f'rasa run --endpoints ./endpoints.yml --credentials ./credentials.yml -m models/prod_model.tar.gz --enable-api -p 5005 --cors "*"'
 
 
 # Chama o comando rasa run a partir do script Python
